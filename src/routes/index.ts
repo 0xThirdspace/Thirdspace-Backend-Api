96186e44a7d106b8b3905fe5f46049befaff9979
olumidtech <olumideadewusis@gmail.com>
2023-07-14 09:14:58 +0100

    bounty & workspace CRUD

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { authRouter } from "./auth/auth.router";
 import { userRouter } from "./user/user.router";
+import workspaceRouter from "./workspace/workspace.router";
+import bountyRouter from "./bounty/bounty.router";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
+router.use("/workspaces", workspaceRouter); 
+router.use("/bounties", bountyRouter);
 
 export default router;
