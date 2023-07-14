61a68630a14a208cd0479dfe4db31a641d6469d9
olumidtech <olumideadewusis@gmail.com>
2023-07-14 09:49:44 +0100

    bounty & workspace CRUD

---
+++ src/routes/workspace/workspace.service.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+class WorkspaceService {
+  static async createWorkspace(userId: string, workspaceName: string, imageUrl?: string) {
+    if (!workspaceName) {
+      throw new Error("You need to provide a workspace name"); 
+      
+    }
+
+    const existingWorkspaceName = await prisma.workspace.findFirst({
+      where: {
+        workspace_name: workspaceName,
+      },
+    });
+
+    if (existingWorkspaceName) {
+      throw new Error("Workspace name already exists"); 
+    }
+
+    const existingWorkspace = await prisma.workspace.findFirst({
+      where: {
+        userId,
+      },
+    });
+
+    if (existingWorkspace) {
+      throw new Error("User can only create a single workspace."); 
+    }
+
+    const workspace = await prisma.workspace.create({
+      data: {
+        workspace_name: workspaceName,
+        userId,
+        imageUrl,
+      },
+      include: {
+        user: false,
+      },
+    });
+
+    return workspace;
+  }
+}
+
+export default WorkspaceService;
