5bc5c874e3ffa9a3643ddac0f9953e9cfe18716c
Adebimpe-Ojo Segun <ojoshegun42@gmail.com>
2023-07-19 17:17:20 +0100

    added image upload to user profile

--- src/routes/user/user.router.ts
+++ src/routes/user/user.router.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
 import authenticateToken from "../../middleware/isAuth";
+import { body, validationResult } from "express-validator";
 
 import * as UserService from "./user.service";
+import upload from "../../middleware/cloudinary";
 
 export const userRouter = express.Router();
 
@@ -34,3 +36,46 @@ userRouter.get(
     }
   }
 );
+
+userRouter.put(
+  "/:id",
+  body("name").isString(),
+  body("email").isString(),
+  authenticateToken,
+  upload.any(),
+  async (request: Request, response: Response) => {
+    const id: string = request.params.id;
+    try {
+      const user = request.body;
+      const files = request.files as Express.Multer.File[];
+      let profileImage = null;
+
+      if (files.length > 0) {
+        const image = files[0];
+        profileImage = image.path;
+      }
+      const updatedUser = await UserService.updateProfile(
+        id,
+        user,
+        profileImage
+      );
+      return response.status(201).json(updatedUser);
+    } catch (error: any) {
+      return response.status(500).json(error.message);
+    }
+  }
+);
+
+userRouter.get("/search", async (request: Request, response: Response) => {
+  try {
+    const { name } = request.query;
+
+    const users = await UserService.searchUser(name);
+
+    console.log("users");
+
+    return response.status(201).json(users);
+  } catch (error: any) {
+    return response.status(500).json(error.message);
+  }
+});
